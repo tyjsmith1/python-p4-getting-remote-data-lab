4d0887e772ee481854d0bff55f7a3ceaf98596a6
tylersmith <tyjsmith1@gmail.com>
2024-01-10 12:41:30 -0600

    done

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -7,7 +7,13 @@ class GetRequester:
         self.url = url
 
     def get_response_body(self):
-        pass
+        response = requests.get(self.url)
+        return response.content
 
     def load_json(self):
-        pass
+        people_list = []
+        people = json.loads(self.get_response_body())
+        for person in people:
+            people_list.append(person)
+        print(people_list)
+        return people_list
